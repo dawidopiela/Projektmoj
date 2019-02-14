1fa79efe6880f64d6d1b3923abfd82a44304ee40
dawidopiela <dawid47918@gmail.com>
2019-02-14 10:39:11 +0100

    piąta wersja

--- src/start/start.java
+++ src/start/start.java
@@ -1,9 +1,12 @@
 package start;
 
+import java.sql.SQLOutput;
+
 public class start {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("JRE version: " + System.getProperty("java.runtime.version"));
 
     }
 
