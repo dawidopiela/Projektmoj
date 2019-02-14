ddc90aaefb78abfbdbae1bcdf7f7e7c838192c74
dawidopiela <dawid47918@gmail.com>
2019-02-14 11:44:04 +0100

    src/Variables/Variables.java

---
+++ src/Variables/Variables.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println(x);
+
+        int y;
+
+        y = x + 11;
+
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+    }
+}
--- src/start/start.java
+++ src/start/start.java
@@ -1,12 +1,49 @@
 package start;
 
-import java.sql.SQLOutput;
+
 
 public class start {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
         System.out.println("JRE version: " + System.getProperty("java.runtime.version"));
+
+        int lunchTimer = 12234;
+        int endTimer = -1234565;
+        System.out.println(lunchTimer);
+        System.out.println(endTimer);
+        double salary = 2100.99;
+        System.out.println(salary);
+
+        char signA = 'A';
+        char sign9 = '9';
+        char signTab = '\'';
+        char signSpace = ' ';
+        System.out.println(signA);
+        System.out.println(sign9);
+        System.out.println(signTab);
+        System.out.println(signSpace);
+
+        boolean flag = true;
+        boolean isLunch = false;
+        System.out.println(true);
+        System.out.println("isLunch =  isLunch");
+        System.out.println("isLunch = " + isLunch);
+
+         String imie1 = "Ola";
+        String  imie2 = "Ala";
+        String  imie3 = "Mia";
+
+
+
+        System.out.println(imie1+ ";"+ imie2+ ";"+ imie3);
+
+
+        
+
+
+
+
 
     }
 
